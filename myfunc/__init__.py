fd52286deec431963bbe83370719ecf02b2b5bc0
Elhay Efrat <elhayefrat@elhays.lan>
2023-02-08 11:55:17 +0200

    tret:wq
    Signed-off-by: Elhay Efrat <elhayefrat@elhays.lan>

--- myfunc/__init__.py
+++ myfunc/__init__.py
@@ -16,6 +16,7 @@ def main(req: func.HttpRequest) -> func.HttpResponse:
         credential = DefaultAzureCredential()
         client = SecretClient(vault_url=KVUri, credential=credential)
         logging.info('UP2')
+        
         # print(f"Retrieving your secret from {secretName}.")
         client.update_secret_properties(secretName, enabled=status)
         if status:
@@ -28,29 +29,8 @@ def main(req: func.HttpRequest) -> func.HttpResponse:
         
         return func.HttpResponse(f"DEBUG.",status_code=200)
 
-        # credential = DefaultAzureCredential()
-        # logging.info('UP')
-        # from azure.keyvault.keys import KeyClient
-        # key_client = KeyClient(vault_url="https://elhaysssss.vault.azure.net/", credential=credential)
-        # logging.info('Python HTTP trigger function processed a request.')
-        # allow_enable = ['eefrat']
-        # enable_status = req.params.get('enable_status')
-        # key = req.params.get('key')
-        # username = req.params.get('username')
-        # if username in allow_enable:
-        #     updated_key = key_client.update_key_properties(key, enabled=True)
-        #     print(updated_key.name)
-        #     print(updated_key.properties.enabled)
-        #     return func.HttpResponse(
-        #          f"Hello, {username}. is not updated the keys.",
-        #          status_code=200
-        #     )
-        # else:
-        #     return func.HttpResponse(f"Hello, {username}. is not allowed.",
-        #                                 status_code=401
-        #     )
     except Exception as Error:
-        return func.HttpResponse(f"ERROR, {Error}. is not allowed.",status_code=401)
+        return func.HttpResponse(f"ERROR, {Error}. is not allowed.",status_code=200)
 
 
 #test
