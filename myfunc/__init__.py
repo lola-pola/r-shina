0d5c480bc4868dfc4e71decb4022dd060aab6faf
Elhay Efrat <elhayefrat@elhays.lan>
2023-02-08 10:49:43 +0200

    dad
    Signed-off-by: Elhay Efrat <elhayefrat@elhays.lan>

--- myfunc/__init__.py
+++ myfunc/__init__.py
@@ -7,20 +7,23 @@ def main(req: func.HttpRequest) -> func.HttpResponse:
     try:
         logging.info('UP')
         from azure.keyvault.secrets import SecretClient
+        logging.info('UP1')
         from azure.identity import DefaultAzureCredential
-        secretName='test'
-        status= True
-        KVUri = f"https://shuval.vault.azure.net/"
-        credential = DefaultAzureCredential()
-        client = SecretClient(vault_url=KVUri, credential=credenti
-        print(f"Retrieving your secret from {secretName}.")
-        client.update_secret_properties(secretName, enabled=status
-        if status:
-            retrieved_secret = client.get_secret(secretName)
-            print(f"Your secret vault is '{retrieved_secret.value}
-            print(" done.")
-        else:
-            print(f"Your secret status is {status} .")
+        logging.info('UP2')
+
+        # secretName='test'
+        # status= True
+        # KVUri = f"https://shuval.vault.azure.net/"
+        # credential = DefaultAzureCredential()
+        # client = SecretClient(vault_url=KVUri, credential=credenti
+        # print(f"Retrieving your secret from {secretName}.")
+        # client.update_secret_properties(secretName, enabled=status
+        # if status:
+        #     retrieved_secret = client.get_secret(secretName)
+        #     print(f"Your secret vault is '{retrieved_secret.value}
+        #     print(" done.")
+        # else:
+        #     print(f"Your secret status is {status} .")
         
         
         return func.HttpResponse(f"DEBUG.",status_code=200)
