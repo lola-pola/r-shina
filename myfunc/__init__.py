43906623a137c1b980bf5cd8ea19d423ae46874a
Elhay Efrat <elhayefrat@elhays-2.lan>
2023-02-14 17:29:12 +0200

    sd
    Signed-off-by: Elhay Efrat <elhayefrat@elhays-2.lan>

--- myfunc/__init__.py
+++ myfunc/__init__.py
@@ -4,51 +4,15 @@ import azure.functions as func
 
 
 def main(req: func.HttpRequest) -> func.HttpResponse:
-    try:
-        logging.info('UP')
-
-        from azure.keyvault.secrets import SecretClient
-        from azure.identity import DefaultAzureCredential , EnvironmentCredential
-        # enable_status = req.params.get('enable_status')
-        # key = req.params.get('key')
-        key = 'test'
-        import os
- 
-        # printing environment variables
-        logging.info('UP1')
-        logging.info(os.environ['AZURE_CLIENT_ID'])
-        logging.info(os.environ['AZURE_TENANT_ID'])
-        logging.info(os.environ['AZURE_CLIENT_ID'])
-        logging.info(os.environ['AZURE_CLIENT_SECRET'])
-        KVUri = f"https://toooo1234.vault.azure.net/"
-        # credential = DefaultAzureCredential()
-        credential = EnvironmentCredential()
-        client = SecretClient(vault_url=KVUri, credential=credential)
-        client.update_secret_properties(key, enabled=True)
+    from azure.keyvault.secrets import SecretClient
+    from azure.identity import DefaultAzureCredential , EnvironmentCredential
+    key = req.params.get('key')
+    kv_url = req.params.get('kv_url')
+    if kv_url == None:
+        kv_url='https://toooo1234.vault.azure.net/'
+    credential = EnvironmentCredential()
+    client = SecretClient(vault_url=kv_url, credential=credential)
+    if client.update_secret_properties(key, enabled=False):
         return func.HttpResponse(f"changed.",status_code=200)
-        # enable_status = bool(enable_status)
-        # secretName= key
-        # status= enable_status
-        # KVUri = f"https://shuval.vault.azure.net/"
-        # credential = DefaultAzureCredential()
-        # client = SecretClient(vault_url=KVUri, credential=credential)
-        # logging.info('UP2')
-
-        # # print(f"Retrieving your secret from {secretName}.")
-        # client.update_secret_properties(secretName, enabled=status)
-        # if status:
-        #     retrieved_secret = client.get_secret(secretName)
-        #     print(f"Your secret vault is {retrieved_secret.value}")
-        #     print(" done.")
-        # else:
-        #     print(f"Your secret status is {status} .")
-
-
-        # return func.HttpResponse(f"DEBUG.",status_code=200)
-
-
-    except Exception as Error:
-        return func.HttpResponse(f"ERROR, {Error}. is not allowed.",status_code=200)
-
-
-#test
+    else:
+        return func.HttpResponse(f"ERROR.",status_code=500)
