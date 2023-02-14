4879afb5892d0181af2abbe7d302f299297f811b
Elhay Efrat <elhayefrat@elhays-2.lan>
2023-02-14 14:20:15 +0200

    sdasd
    Signed-off-by: Elhay Efrat <elhayefrat@elhays-2.lan>

--- myfunc/__init__.py
+++ myfunc/__init__.py
@@ -12,6 +12,11 @@ def main(req: func.HttpRequest) -> func.HttpResponse:
         enable_status = req.params.get('enable_status')
         key = req.params.get('key')
         logging.info('UP1')
+        
+        KVUri = f"https://elhayelhayelhayelhay.vault.azure.net/"
+        credential = DefaultAzureCredential()
+        client = SecretClient(vault_url=KVUri, credential=credential)
+        client.update_secret_properties(key, enabled=True)
         return func.HttpResponse(f"changed.",status_code=200)
         # enable_status = bool(enable_status)
         # secretName= key
